94825acf9ff40558bc31e8f388ec0757ba6dde1d
Sanae_Laklai <Sanae.laklai@gmai.com>
2025-04-10 11:21:48 -0400

    complete ShoppingList app

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apple");
+        System.out.println("flowers");
+        System.out.println("Milk");
+        System.out.println("Chicken");
+        System.out.println("Pasta");
+        System.out.println("Vegetables");
+        System.out.println("Cheese");
+        System.out.println("Rice");
+        System.out.println("Bananas");
+        System.out.println("Eggs");
+    }
+}
